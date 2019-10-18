29f38039cd099838258782c32d27916830ec1c8f
name <1822163549@qq.com>
2019-10-18 19:19:23 +0800

    制作一个本网页的页面
    添加了about.html

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,4 +10,6 @@ def count(request):
     user_text = request.GET['text']
     total_count = len(request.GET['text'])
     return render(request, 'count.html', {'count':total_count, 'text':user_text} )
-    
+
+def about(request):
+	return render(request, 'about.html')
